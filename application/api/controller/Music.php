85e27e828ed3c7473291bf9e81271cbc7075067a
wxfbboy <2213451361@qq.com>
2020-08-17 14:28:01 +0800

    添加播放器页面1

--- application/api/controller/Music.php
+++ application/api/controller/Music.php
@@ -32,7 +32,7 @@ class Music extends Controller
             $this->_meting = new Meting(self::$$platform_str);
             switch (self::$$platform_str){
                 case 'tencent':
-                    $this->_meting->cookie('pgv_pvi=6086360064; pgv_si=s887317504; pgv_pvid=2050129028; yplayer_open=0; ts_last=y.qq.com/portal/player.html; ts_uid=8405946048; yq_index=0; qqmusic_fromtag=66; player_exist=1')
+                    $this->_meting->cookie('pgv_pvi=6086360064; pgv_si=s887317504; pgv_pvid=2050129028; yplayer_open=0; ts_last=y.qq.com/portal/player.html; ts_uid=8405946048; yq_index=0; qqmusic_fromtag=66; player_exist=1');
                     break;
             }
         }
